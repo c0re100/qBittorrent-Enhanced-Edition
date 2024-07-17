4167f3c9a1cb8ddacc35db3c0ddc0d8812bc7b68
KagurazakaNyaa <dev@kagurazakanyaa.com>
2024-07-17 09:25:03 +0800

    add shadowban state to peerinfo

--- src/base/bittorrent/peerinfo.h
+++ src/base/bittorrent/peerinfo.h
@@ -62,6 +62,8 @@ namespace BitTorrent
         bool isOnParole() const;
         bool isSeed() const;
 
+        bool isShandowBanned() const;
+
         bool optimisticUnchoke() const;
         bool isSnubbed() const;
         bool isUploadOnly() const;
